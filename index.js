a7530e37edd0b919b6d3a3fe5db88c5f688426c6
Jouni Juntunen <jouni.juntunen@oamk.fi>
2024-02-27 08:55:08 +0200

    env vault

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+//https://www.dotenv.org/docs/languages/nodejs/render
+// npx dotenv-vault@latest new
+// npx dotenv-vault@latest login
+// npx dotenv-vault@latest push
+// npx dotenv-vault@latest open production
+// npx dotenv-vault@latest build
+// commit to git
+
+require('dotenv').config()
+console.log(process.env)
 const port = process.env.PORT || 3000
 const http = require('http')
 const server = http.createServer((req,res) => {
   res.statusCode = 200
   res.setHeader('Content-Type','text/plain')
-  res.end(`Hello ${process.env.PORT}`)
+  res.end(`Hello ${process.env.HELLO}`)
 })
 
 server.listen(port,() => {
